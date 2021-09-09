87ccf0a2e4b5e160f8ae53987acc97aad49d9d00
gyongyiovics <gyongyimako@gmail.com>
2021-09-09 14:06:10 +0200

    added const, getter-setter to Ship class

--- src/main/java/models/Ship.java
+++ src/main/java/models/Ship.java
@@ -7,9 +7,60 @@ import java.util.Map;
 public class Ship {
     private String name;
     private int attackPower;
-    private int state = 100;
+    private int state;
     private final String weapon = "CANNON";
     private final int cannonDamage = 25;
-    private Map<String, List<Pirate>> shipWithCrew = new HashMap<>();
+    private Map<String, List<Pirate>> shipWithCrew;
 
+    public Ship(String name, int attackPower, int state, Map<String, List<Pirate>> shipWithCrew) {
+        this.name = name;
+        this.attackPower = attackPower;
+        this.state = state;
+        this.shipWithCrew = shipWithCrew;
+    }
+
+    public int attack(Ship enemy) {
+        enemy.setAttackPower(enemy.getAttackPower() - this.getCannonDamage());
+        return enemy.getAttackPower();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAttackPower() {
+        return attackPower;
+    }
+
+    public void setAttackPower(int attackPower) {
+        this.attackPower = attackPower;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public int getCannonDamage() {
+        return cannonDamage;
+    }
+
+    public Map<String, List<Pirate>> getShipWithCrew() {
+        return shipWithCrew;
+    }
+
+    public void setShipWithCrew(Map<String, List<Pirate>> shipWithCrew) {
+        this.shipWithCrew = shipWithCrew;
+    }
 }
